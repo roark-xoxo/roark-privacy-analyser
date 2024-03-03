108631a6d0ba14dcb28f55118e8a79ba1aa11e32
marcellocurto <marcello@roark.at>
2024-03-03 13:35:36 +0100

    update tests

--- tests/lib/scraper/email.test.js
+++ tests/lib/scraper/email.test.js
@@ -3,14 +3,14 @@ import { getEmailAddressesFromHTML } from "../../../lib/scraper/email.js";
 
 test("Filters emails from HTML string", () => {
   const string =
-    "mailto:roark@roark.at über@uber.com hi@---@hi mailto:über@über.de ä@b.com marcello.curto.mail@mail.com mail@google.google.com mail@longtld.website img-2x@me.jpg";
+    "mailto:roark@roark.at über@uuber.com hi@---@hi mailto:über@über.de ä@b.com marcello.curto.mail@mail.com mail@goo.goo.com mail@longtld.website img-2x@me.jpg";
   const addresses = getEmailAddressesFromHTML(string);
   expect(addresses).toEqual([
     "roark@roark.at",
-    "über@uber.com",
+    "über@uuber.com",
     "ä@b.com",
     "marcello.curto.mail@mail.com",
-    "mail@google.google.com",
+    "mail@goo.goo.com",
     "mail@longtld.website",
   ]);
 });
